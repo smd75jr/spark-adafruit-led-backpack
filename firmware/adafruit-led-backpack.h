30206770f920e61605ea29863470495ebec16013
Stephen DeLay <smd75jr@gmail.com>
2016-03-06 14:37:02 -0500

    Update adafruit-led-backpack.h

--- firmware/adafruit-led-backpack.h
+++ firmware/adafruit-led-backpack.h
@@ -74,17 +74,6 @@ class Adafruit_LEDBackpack {
   uint8_t i2c_addr;
 };
 
-class Adafruit_AlphaNum4 : public Adafruit_LEDBackpack {
- public:
-  Adafruit_AlphaNum4(void);
-
-  void writeDigitRaw(uint8_t n, uint16_t bitmask);
-  void writeDigitAscii(uint8_t n, uint8_t ascii, boolean dot = false);
-
- private:
-
-
-};
 
 class Adafruit_24bargraph : public Adafruit_LEDBackpack {
  public:
@@ -93,73 +82,4 @@ class Adafruit_24bargraph : public Adafruit_LEDBackpack {
   void setBar(uint8_t bar, uint8_t color);
 
  private:
-};
-
-
-class Adafruit_8x16matrix : public Adafruit_LEDBackpack, public Adafruit_GFX {
- public:
-  Adafruit_8x16matrix(void);
-
-  void drawPixel(int16_t x, int16_t y, uint16_t color);
-
- private:
-};
-
-class Adafruit_8x8matrix : public Adafruit_LEDBackpack, public Adafruit_GFX {
- public:
-  Adafruit_8x8matrix(void);
-
-  void drawPixel(int16_t x, int16_t y, uint16_t color);
-
- private:
-};
-
-class Adafruit_BicolorMatrix : public Adafruit_LEDBackpack, public Adafruit_GFX {
- public:
-  Adafruit_BicolorMatrix(void);
-
-  void drawPixel(int16_t x, int16_t y, uint16_t color);
-
- private:
-};
-
-
-#define DEC 10
-#define HEX 16
-#define OCT 8
-#define BIN 2
-#define BYTE 0
-
-class Adafruit_7segment : public Adafruit_LEDBackpack {
- public:
-  Adafruit_7segment(void);
-  size_t write(uint8_t c);
-
-  void print(char, int = BYTE);
-  void print(unsigned char, int = BYTE);
-  void print(int, int = DEC);
-  void print(unsigned int, int = DEC);
-  void print(long, int = DEC);
-  void print(unsigned long, int = DEC);
-  void print(double, int = 2);
-  void println(char, int = BYTE);
-  void println(unsigned char, int = BYTE);
-  void println(int, int = DEC);
-  void println(unsigned int, int = DEC);
-  void println(long, int = DEC);
-  void println(unsigned long, int = DEC);
-  void println(double, int = 2);
-  void println(void);
-
-  void writeDigitRaw(uint8_t x, uint8_t bitmask);
-  void writeDigitNum(uint8_t x, uint8_t num, boolean dot = false);
-  void drawColon(boolean state);
-  void printNumber(long, uint8_t = 2);
-  void printFloat(double, uint8_t = 2, uint8_t = DEC);
-  void printError(void);
-
-  void writeColon(void);
-
- private:
-  uint8_t position;
 };
